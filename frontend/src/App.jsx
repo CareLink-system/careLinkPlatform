094fe50be3ef788ff49e76dbe42d8fe261934a58
Shalon Fernando <shalonfernando52@gmail.com>
2026-03-26 21:40:49 +0530

    ref: add routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import LoginPage from './features/auth/LoginPage'
 import RegisterPage from './features/auth/RegisterPage'
 import DashboardPage from './pages/DashboardPage'
 import TelemedicinePage from './pages/TelemedicinePage'
+import SymptomCheckerPage from './pages/SymptomCheckerPage'
 import ProtectedRoute from './components/dashboard/ProtectedRoute'
 import DashboardShell from './components/dashboard/DashboardShell'
 import './App.css'
@@ -19,6 +20,7 @@ function App() {
       <Route element={<DashboardShell />}>
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/telemedicine/:appointmentId" element={<TelemedicinePage />} />
+        <Route path="/symptom-checker" element={<SymptomCheckerPage />} />
       </Route>
     </Routes>
   )
