11655d835075ac400a126069d513dd7bfade3760
DinilDulneth <dinildulneth123@gmail.com>
2026-04-04 11:23:11 +0530

    secure all the service

---
+++ backend/Services/PaymentService/DependencyInjection.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using PaymentService.Data;
+using PaymentService.Services;
+
+namespace PaymentService;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection RegisterServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddHttpContextAccessor();
+
+        services.AddDbContext<PaymentDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped<IPaymentService, PaymentService.Services.PaymentService>();
+        services.AddScoped<Repositories.IPaymentRepository, Repositories.PaymentRepository>();
+
+        return services;
+    }
+}
