c2e43a9023b17ed659c3e5b7d882614516b599b5
Your <yrukshan345@gmail.com>
2026-04-09 15:01:16 +0530

    Patient Service Backend Commit
    Patient Service, Patient Profile, Patient Medical Report Upload, all , Commit , Backend

--- backend/Services/PatientService/Models/Patient.cs
+++ backend/Services/PatientService/Models/Patient.cs
@@ -1,16 +1,15 @@
 namespace PatientService.Models;
 
-public class Patient 
+using PatientService.Models.Common;
+using System;
+
+public class Patient : AuditableEntity
 {
     public int Id { get; set; }
-    public int UserId { get; set; } // Links to AuthService user
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Phone { get; set; }
-    public DateTime DateOfBirth { get; set; }
-    public string BloodGroup { get; set; }
-
-    //Temparary commont for now, will be removed later
-    //public List<MedicalReport> MedicalReports { get; set; }
-
+    public Guid UserId { get; set; } // Links to AuthService user
+    public string FullName { get; set; } = default!;
+    public string Phone { get; set; } = default!;
+    public string DateOfBirth { get; set; } = default!; // yyyy-MM-dd format
+    public string Gender { get; set; } = default!;
+    public string BloodGroup { get; set; } = default!;
 }
