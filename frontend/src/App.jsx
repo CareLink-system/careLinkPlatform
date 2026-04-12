ff2e898010be2e5546ead756e084c16fd8f507d3
DinilDulneth <dinildulneth123@gmail.com>
2026-04-12 13:32:33 +0530

    new changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import LandingPage from './features/landing/LandingPage'
-import LoginPage from './features/auth/LoginPage'
-import RegisterPage from './features/auth/RegisterPage'
-import DashboardPage from './pages/PatientDashboardPage'
-import DoctorDashboardPage from './pages/DoctorDashboardPage'
-import AdminDashboardPage from './pages/AdminDashboardPage'
-import TelemedicinePage from './pages/TelemedicinePage'
-import SymptomCheckerPage from './pages/SymptomCheckerPage'
-import DashboardShell from './components/dashboard/DashboardShell'
-import './App.css'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./features/landing/LandingPage";
+import LoginPage from "./features/auth/LoginPage";
+import RegisterPage from "./features/auth/RegisterPage";
+import DashboardPage from "./pages/PatientDashboardPage";
+import DoctorDashboardPage from "./pages/DoctorDashboardPage";
+import AdminDashboardPage from "./pages/AdminDashboardPage";
+import UserManagementPage from "./features/userManagement/pages/userManagementPage";
+import TelemedicinePage from "./pages/TelemedicinePage";
+import SymptomCheckerPage from "./pages/SymptomCheckerPage";
+import BasicFinancialTransactionMonitoringPage from "./features/paymentTransaction/pages/basicFinancialTransactionMonitoringPage";
+import DashboardShell from "./components/dashboard/DashboardShell";
+import "./App.css";
 
 function App() {
   return (
@@ -22,11 +24,19 @@ function App() {
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/doctor-dashboard" element={<DoctorDashboardPage />} />
         <Route path="/admin-dashboard" element={<AdminDashboardPage />} />
-        <Route path="/telemedicine/:appointmentId" element={<TelemedicinePage />} />
+        <Route path="/admin/users" element={<UserManagementPage />} />
+        <Route
+          path="/payments"
+          element={<BasicFinancialTransactionMonitoringPage />}
+        />
+        <Route
+          path="/telemedicine/:appointmentId"
+          element={<TelemedicinePage />}
+        />
         <Route path="/symptom-checker" element={<SymptomCheckerPage />} />
       </Route>
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
